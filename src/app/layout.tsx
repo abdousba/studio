a86b667450326cc53fb31cdfef32b3a629c9a95e
Abdelilah Boubchir <boubchirabdelilah@gmail.com>
2026-01-22 06:59:56 +0000

    نمط off lineلا يعمل هل يمكنك التحقق

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="PharmaSuivi" />
       </head>
       <body
         className={cn(
